57f2b8dd3af49352a59b71426231bd72cc2aa2b5
DmitryUgryumov <dmitryugryumov13@gmail.com>
2023-02-13 13:18:03 +0500

    chore: add eslint, prettier, stylelint

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
